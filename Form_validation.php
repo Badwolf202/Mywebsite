e47f6cd4c27f2140893595fb59675fc4b52150f3
Badwofl202 <blake.william@myokanagan.bc.ca>
2021-12-03 21:06:23 -0800

    Version 2

--- Form_validation.php
+++ Form_validation.php
@@ -12,32 +12,33 @@ if (!isset($_POST['submit']))
 
 ?>
 
-<html>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<head>
-    <title>User Login</title>
-</head>
+    <html>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <head>
+        <title>User Login</title>
+    </head>
 
-</div>
-<div class="container-fluid text-center">
-    <fieldset> <legend><h3> Account creation form </h3></legend>
-        <form method="post" action="Form_validation.php">
-            <p class="form-check-input" style="color: white"><strong>First Name:</strong><br>
+    </div>
+    <div class="container-fluid text-center">
+        <legend><h3 class="h3 mb-3" style="color: white"> Account creation form </h3></legend>
+        <form method="post" action="Form_validation.php" class="form-floating">
+            <p class="text-center" style="color: white"><strong>First Name:</strong><br>
                 <input type="text" name="firstname" required></p>
-            <p class="form-check-input" style="color: white"><strong>Last Name:</strong><br>
+            <p class="" style="color: white"><strong>Last Name:</strong><br>
                 <input type="text"  name="lastname" required></p>
-            <p class="form-check-input" style="color: white"><strong>Email:</strong><br>
+            <p class="" style="color: white"><strong>Email:</strong><br>
                 <input type="text"  name="email" required></p>
             <p><input class="btn btn-outline-primary" type="submit" name="submit" value="Add user"/></p>
         </form>
-    </fieldset>
-    <a class="btn btn-outline-primary" href="index.html">HOME</a>
-    <a  class="btn btn-outline-primary" href="FormAndReport.php">User Tables</a>
-    </body>
-</div>
-<body>
+        <a class="btn btn-outline-primary" href="index.html">HOME</a>
+        <br>
+        <br>
+        <a  class="btn btn-outline-primary" href="FormAndReport.php">User Tables</a>
+        </body>
+    </div>
+    <body>
 
-</html>
+    </html>
 
 <?php
 }else{
