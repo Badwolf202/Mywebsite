09f181ce1cc3e9d9fb2075d9cef3228704eb5e83
Badwofl202 <blake.william@myokanagan.bc.ca>
2021-12-13 10:47:18 -0800

    Really again :(

--- HTML/Resume.php
+++ HTML/Resume.php
@@ -1,4 +1,4 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <?php
 session_start();
 ?>
@@ -16,10 +16,10 @@ session_start();
     <?php
 
     if(!empty($_SESSION['logged_in'])){
-        echo "<a class='btn btn-outline-danger my-2 my-sm-0' href='../PHP/logout.php' role='button'>Logout</a>";
+        echo "<a class='btn btn-outline-danger my-2 my-sm-0' href='PHP/logout.php' role='button'>Logout</a>";
     }else{
-        echo "<a class='btn btn-outline-primary my-2 my-sm-0 ' href='../PHP/applyaccount.php' role='button'>Register</a>";
-        echo "<a class='btn btn-outline-primary my-2 my-sm-0' href='../HTML/Login.html' role='button'>Login</a>";
+        echo "<a class='btn btn-outline-primary my-2 my-sm-0 ' href='PHP/applyaccount.php' role='button'>Register</a>";
+        echo "<a class='btn btn-outline-primary my-2 my-sm-0' href='HTML/Login.html' role='button'>Login</a>";
     }
     ?>
 
@@ -34,31 +34,25 @@ session_start();
             <hr>
             <ul class="nav nav-pills flex-column mb-auto">
                 <li class="nav-item">
-                    <a href="../HTML/JQuery.php" class="nav-link active" aria-current="page">
+                    <a href="HTML/JQuery.php" class="nav-link" aria-current="page">
                         <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"/></svg>
                         JQuery
                     </a>
                 </li>
                 <li>
-                    <a href="../HTML/AJAX.php" class="nav-link text-white">
+                    <a href="HTML/AJAX.php" class="nav-link text-white">
                         <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg>
                         AJAX
                     </a>
                 </li>
                 <li>
-                    <a href="../PHP/Form_validation.php" class="nav-link text-white">
-                        <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"/></svg>
-                        Form Validation
-                    </a>
-                </li>
-                <li>
-                    <a href="../PHP/FormAndReport.php" class="nav-link text-white">
+                    <a href="PHP/sqltableresults.php" class="nav-link text-white">
                         <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg>
-                        Report
+                        All Reviews
                     </a>
                 </li>
                 <li>
-                    <a href="../HTML/Resume.php" class="nav-link text-white">
+                    <a href="HTML/Resume.php" class="nav-link text-white">
                         <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg>
                         Resume
                     </a>
@@ -68,16 +62,16 @@ session_start();
                 if(!empty($_SESSION['logged_in'])){
                     ?>
                     <li>
-                        <a href="Resume.php" class="nav-link text-white">
+                        <a href="PHP/MakeReview.php" class="nav-link text-white">
                             <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg>
-                            Resume
+                            Make Review
                         </a>
                     </li>
 
                     <li>
-                        <a href="Resume.php" class="nav-link text-white">
+                        <a href="HTML/Holder.html" class="nav-link text-white">
                             <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg>
-                            Resume
+                            Holder (Tester Page)
                         </a>
                     </li>
                     <?php
@@ -89,9 +83,79 @@ session_start();
 
 
 </div>
+<style>
+    .h2,p,.h1,.h3{
+        color: white;
+    }
+</style>
 <article class="container-sm" style="padding-left: 400px; padding-right: 200px; margin: 15px">
 
-</article>
+    <h1 class="h1">Noah Blake</h1>
+    <br>
+    <h2 class="h2">PROFILE</h2><br>
+    <p>
+        Since I was young, I have been consistently working towards building a future
+        for myself. While working I was always appreciated for my hard work and
+        willingness to do what it took for the team to succeed
+
+    </p>
+    <br>
+    <h2 class="h2">EXPERIENCE</h2><br>
+    <h3 class="h3">McDonald’s Team Leader | Penticton | 2020 – Present</h3>
+    <p>
+        ▪ Participate in training of new Crew members.<br>
+        ▪ Opening and closing the building.<br>
+        ▪ Takes pride in work.<br>
+        ▪ Detail cleaning:enjoys workinginan organized,maintained workspace.<br>
+        ▪ Activelyinvolvedinthedailymaintenanceoftheequipment:grill cleaning,Fryer
+        maintenance, etc.<br>
+        ▪ Responsible for overseeing the building<br>
+    </p>
+    <br>
+    <h3 class="h3">Landmark Cinemas | Penticton | 2014 – 2020</h3>
+    <p>
+        ▪ InvolvedinPOSandTheatrecomputersetupandproblemsolving.<br>
+        ▪ Participate in training of new Cast members.<br>
+        ▪ Proficientin handlinghigh volumes ofpatrons professionally.<br>
+        ▪ Opening and closing the building.<br>
+        ▪ Takes pride in work.<br>
+        ▪ Skilled at cash handling and accounting.<br>
+        ▪ Detail cleaning:enjoys workinginan organized,maintained workspace.<br>
+        ▪ Activelyinvolvedinthedailymaintenanceofthebuilding: seat repair,concession
+        maintenance, light changing, etc.
+
+    </p>
+    <br>
+    <h2 class="h2">CERTIFICATIONS</h2><br>
+    <p>
+        ▪Food Safe Level #1
+        ▪W.H.M.I.S.
+        ▪Gateway to technology
+        program
+    </p>
+    <br>
+    <h2 class="h2">REFERENCES</h2><br>
+    <fieldset style="color: white" class="float-left">
+        <p>
+        <h4 class="h4">William Afton</h4><br>
+        Manager,
+        Fazbear entertainment<br>
+        Penticton, BC<br>
+        613-555-0199<br>
+
+        </p>
+    </fieldset>
+    <fieldset style="color: white; padding-left: 50px" class="float-left">
+        <p>
+        <h4 class="h4">Michel Scott</h4><br>
+        Manager,
+        Dunder Mifflin<br>
+        Scranton, PA<br>
+        613-555-0111<br>
+
+        </p>
+    </fieldset>
+
 </article>
 
 
