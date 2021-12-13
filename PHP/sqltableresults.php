55a74a138367c09a39d3bd976a03db76df45dc53
Badwofl202 <blake.william@myokanagan.bc.ca>
2021-12-13 00:14:26 -0800

    Really again :(

--- PHP/sqltableresults.php
+++ PHP/sqltableresults.php
@@ -1,7 +1,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <?php
 echo "Hello From the PHP";
-$mysqli = new mysqli("localhost", "root", "letmein", "testDB");
+$mysqli = new mysqli("159.223.103.70", "root", "letmein", "testDB");
 $data = mysqli_query($mysqli, "SELECT * from `gameRev`;");
 echo "2";
 if (!$mysqli -> query("SELECT * from `gameRev`;")) {
