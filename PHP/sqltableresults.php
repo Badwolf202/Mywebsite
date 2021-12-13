b61a01675e6bb3e92be911a7a4920ca49dbb95ce
Badwofl202 <blake.william@myokanagan.bc.ca>
2021-12-13 00:04:40 -0800

    Really again :(

--- PHP/sqltableresults.php
+++ PHP/sqltableresults.php
@@ -1,10 +1,9 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
 <?php
 $mysqli = new mysqli("localhost", "root", "letmein", "testDB");
 $data = mysqli_query($mysqli, "SELECT * from `gameRev`;");
 echo "<style>td{border: 1px solid black; color:blue}, tr {border: 1px solid black; color:blue}</style>";
-echo "<table class='table'>";
+echo "<table>";
 echo "<thead><tr><td>Game</td><td>Rating</td><td>Notes</td></tr></thead>";
 //write out the current ticket
 echo "<tbody>";
