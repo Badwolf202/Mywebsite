6b46d8624f5e270cea868a38ba15f2e6e253912f
Badwofl202 <blake.william@myokanagan.bc.ca>
2021-12-13 10:51:35 -0800

    Really again :(

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ session_start();
     <div class="w-auto h-auto container-fluid">
         <section class="btn-toolbar position-fixed h-100 float-left">
             <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
-                <a href="../index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                <a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                     <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
                     <span class="fs-4">Noah's Site</span>
                 </a>
