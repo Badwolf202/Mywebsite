6665f16d46c9a7023b7a9c1578024dbfe7bb3934
Badwofl202 <blake.william@myokanagan.bc.ca>
2021-12-13 00:33:14 -0800

    Really again :(

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
                 <hr>
             </div>
 
-    <div class="container-fluid text-center float-none">
+    <div class="container-fluid text-center float-right">
         <?php
 $mysqli = new mysqli("localhost", "noahb", "letmein", "testDB");
 $data = mysqli_query($mysqli, "SELECT * FROM `gameRev`;");
