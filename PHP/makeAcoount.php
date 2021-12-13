fae07b6565ca2a25c21e09533e2b741d99e7bf02
Badwofl202 <blake.william@myokanagan.bc.ca>
2021-12-13 02:13:10 -0800

    Really again :(

--- PHP/makeAcoount.php
+++ PHP/makeAcoount.php
@@ -4,7 +4,7 @@ $email = filter_input(INPUT_POST, 'email');
 $password = filter_input(INPUT_POST, 'password');
 $lowerEmail = strtolower($email);
 
-    $check = mysqli_query($mysqli, "SELECT * FROM `user` WHERE email = '$lowerEmail'");
+    $check = mysqli_query($mysqli, "SELECT * FROM `users` WHERE email = '$lowerEmail'");
     if(empty($email) || empty($password)){
         echo "one of the options was empty, please try again";
     } else if (mysqli_num_rows($check) != 0)
@@ -14,7 +14,7 @@ $lowerEmail = strtolower($email);
     }
     else
     {
-        $sql = "INSERT INTO `user` (`email`,`pass`) VALUES ('$lowerEmail',SHA1('$password'))";
+        $sql = "INSERT INTO `users` (`email`,`pass`) VALUES ('$lowerEmail',SHA1('$password'))";
         if (mysqli_query($mysqli, $sql))
         {
             echo "<h3 class='h3 mb-3 text-center' style='text-align: center'>Your Account has been created! Thank you for joining us!<h3>";
