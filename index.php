05649039032283d7432d825db91d78c6179c0ca3
Badwofl202 <blake.william@myokanagan.bc.ca>
2021-12-12 23:31:37 -0800

    Really 2

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
                 <hr>
             </div>
 
-
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <div class="container-fluid text-center position-fixed">
          <?php
 
@@ -95,4 +95,5 @@
     </section>
 </div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
 </html>
