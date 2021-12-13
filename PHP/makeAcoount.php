d500d69be0a06ae0ea427027c23ad531733d0cce
Badwofl202 <blake.william@myokanagan.bc.ca>
2021-12-13 10:26:10 -0800

    Really again :(

--- PHP/makeAcoount.php
+++ PHP/makeAcoount.php
@@ -29,7 +29,7 @@ $lowerEmail = strtolower($email);
     <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/sign-in/">
     <title>Created!</title>
 </head>
-<body>
+<body class="bg-dark">
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
 </html>
